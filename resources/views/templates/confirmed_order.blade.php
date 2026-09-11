1e251c286885f6bdccadf80eea1705939f22e488
Alan GIRALT <alangiralt@telefonica.net>
2026-09-12 00:06:34 +0200

    clean: remove obsolete commented Blade code from confirmed order line template

--- resources/views/templates/confirmed_order.blade.php
+++ resources/views/templates/confirmed_order.blade.php
@@ -5,21 +5,11 @@
         </div>
         <div class="grid grid-cols-12 gap-4 items-center">
             <div class="line-reference col-span-2 text-xs text-black tracking-wide"></div>
-            <div class="line-dimensions col-span-2 text-right font-normal tracking-wide text-black whitespace-nowrap">
-                <!-- @if((int)$product->height === -1)
-                    Ø {{ (int)$product->width }} × {{ (int)$product->length }}
-                @else
-                    {{ (int)$product->width }} × {{ (int)$product->height }} × {{ (int)$product->length }}
-                @endif -->
-            </div>
+            <div class="line-dimensions col-span-2 text-right font-normal tracking-wide text-black whitespace-nowrap"></div>
             <div class="col-span-2 text-right font-normal"></div>
             <div class="line-quantity col-span-2 text-center font-normal flex justify-center"></div>
-            <div class="line-unit-price col-span-2 tracking-wide whitespace-nowrap">
-                <!-- {{ number_format($product->unit_price, 2, ',', '.') }} {{ $product->unit }} -->
-            </div>
-            <div class="line-subtotal col-span-2 text-right font-bold text-black tracking-wide">
-                <!-- {{ number_format($product->subtotal, 2, ',', '.') }} € -->
-            </div>
+            <div class="line-unit-price col-span-2 tracking-wide whitespace-nowrap"></div>
+            <div class="line-subtotal col-span-2 text-right font-bold text-black tracking-wide"></div>
         </div>
     </div>
 </template>
